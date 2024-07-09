9fb0ac54a007a3b0490d5c55ae65dba40bd1360d
raji-nasrallah <raji.nasrallah@bsqrtech.com>
2024-07-09 11:19:00 +0100

    Update todolistc.js

--- todo-list cookies/todolistc.js
+++ todo-list cookies/todolistc.js
@@ -43,7 +43,7 @@ function setCookie(key, value, time) {
 
 // Function to get the value of a cookie by name
 function getCookie(name) {
-    const value = `; ${document.cookie}`;
+    const value = ";" + document.cookie;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return decodeURIComponent(parts.pop().split(';').shift());//makes it correctly formatted
 }
